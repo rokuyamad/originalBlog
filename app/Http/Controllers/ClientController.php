b06e792fe2ce3ba553ef46fb2d844ed48b01841c
Daiki Rokuyama <dr19950228@gmail.com>
2018-02-14 21:04:45 +0900

    add some code for markup

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
 
 class ClientController extends Controller
 {
     public function index()
     {
-        return view('client.index');
+        $posts = Post::orderBy('id', 'ASC')->paginate(8);
+        return view('client.index')->with('posts', $posts);
     }
 }
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,39 +1,24 @@
 @extends('layout')
 
 @section('content')
-  <div id="profile" class="col-md-2 column">
-    <div class="outer">
-      <div class="picture">
-        <img src="{{ asset("image/avatars/sample.png") }}" alt="">
+
+  <div class="row">
+    @foreach ($posts as $post)
+      <div class="col-md-3" style="animation-duration: 1500ms; opacity: 1;">
+        <img src="{{ asset("/image/topImages/{$post->top_image}") }}" alt="">
+        <div class="button">
+          <button type="button" class="btn btn-primary btn-xs">{{ $post->category->category_name }}</button>
+          @foreach ($post->tags as $tag)
+            <button type="button" class="btn btn-success btn-xs">{{ $tag->tag_name }}</button>
+          @endforeach
+        </div>
+        <h2>
+          <a href="/">{{ $post->title }}</a>
+        </h2>
       </div>
-      <div class="mynameis">
-        <p>DAIKI ROKUYAMA</p>
-      </div>
-      <div class="links">
-        <ul>
-          <li class="twitter">
-            <a href="https://twitter.com/DRokuyama" style="color:#55acee"><i class="fab fa-twitter-square"></i>Twitter</a>
-          </li>
-          <li class="facebook">
-            <a href="https://www.facebook.com/daiki.rokuyama" style="color:#315096"><i class="fab fa-facebook-square"></i>facebook</a>
-          </li>
-          <li>
-            <a href="https://github.com/daikirokuyama" style="color:black"><i class="fab fa-github-square"></i>Github</a>
-          </li>
-          <li>
-            <a href="mailto:dr19950228@gmail.com" style="color:black"><i class="fas fa-envelope"></i>Mail</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    @endforeach
+    {{ $posts->render() }}
   </div>
-  <div class="col-md-3 column">
-    <h1 class="category">IT</h1>
-  </div>
-  <div class="col-md-3 column">
-    <h1 class="category">English</h1>
-  </div>
-  <div class="col-md-3 column">
-    <h1 class="category">Diary</h1>
-  </div>
+
+
 @endsection
