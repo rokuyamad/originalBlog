5a74b1c5a9a0377d7a962a3f076ce960aeba4f89
Daiki Rokuyama <dr19950228@gmail.com>
2018-02-16 17:46:42 +0900

    create partials directory and show file

---
+++ resources/views/client/show.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>rokuyama.daiki</title>
+  <!-- Bootstrap -->
+  <link href="/packages/admin/AdminLTE/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+  <div class="main">
+    <div class="article">
+      <div class="eyecatch-cover">
+        <div class="eyecatch">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="/packages/admin/AdminLTE/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
