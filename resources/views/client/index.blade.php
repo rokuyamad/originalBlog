25c9bfa8c7d4f084e6a6d635514ed6d53cd51bfb
Daiki Rokuyama <dr19950228@gmail.com>
2018-02-21 23:35:26 +0900

    modify layout

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -5,18 +5,18 @@
   <div class="parent-article">
     @foreach ($posts as $post)
       <article>
-        <div class="thumbnail-wrap adjust-box">
-          <div class="thumbnail-image" style="background-image:url({{ asset("/image/topImages/{$post->top_image}") }}")></div>
-        </div>
-        <div class="button">
-          <button class="btn btn-primary btn-xs">{{ $post->category->category_name }}</button>
-          @foreach ($post->tags as $tag)
-            <button class="btn btn-success btn-xs">{{ $tag->tag_name }}</button>
-          @endforeach
-        </div>
-        <h2>
-          <a id="showPage" href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-        </h2>
+        <a href="/posts/{{ $post->id }}">
+          <div class="thumbnail-wrap adjust-box">
+            <div class="thumbnail-image" style="background-image:url({{ asset("/image/topImages/{$post->top_image}") }}")></div>
+          </div>
+          <div class="button">
+            <button class="btn btn-primary btn-xs">{{ $post->category->category_name }}</button>
+            @foreach ($post->tags as $tag)
+              <button class="btn btn-success btn-xs">{{ $tag->tag_name }}</button>
+            @endforeach
+          </div>
+          <h2>{{ $post->title }}</h2>
+        </a>
       </article>
     @endforeach
     <div id="paginaton">
