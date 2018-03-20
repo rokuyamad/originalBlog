b8da44b5670485d22244d8751c530414d252d663
Daiki Rokuyama <dr19950228@gmail.com>
2018-03-20 15:52:57 +0900

    modify little

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -5,18 +5,20 @@
   <div class="parent-article animsition">
     @foreach ($posts as $post)
       <article>
-        <a href="/posts/{{ $post->id }}">
-          <div class="thumbnail-wrap adjust-box">
-            <div class="thumbnail-image" style="background-image:url({{ asset("/image/topImages/{$post->top_image}") }}")></div>
-          </div>
-          <div class="button">
-            <button class="btn btn-primary btn-xs">{{ $post->category->category_name }}</button>
-            @foreach ($post->tags as $tag)
-              <button class="btn btn-success btn-xs">{{ $tag->tag_name }}</button>
-            @endforeach
-          </div>
-          <h2>{{ $post->title }}</h2>
-        </a>
+        <div class="thumbnail-wrap adjust-box">
+          <div class="thumbnail-image" style="background-image:url({{ asset("/image/topImages/{$post->top_image}") }}")></div>
+        </div>
+
+        <div class="button">
+          <button class="btn btn-primary btn-xs">{{ $post->category->category_name }}</button>
+          @foreach ($post->tags as $tag)
+            <button class="btn btn-success btn-xs">{{ $tag->tag_name }}</button>
+          @endforeach
+        </div>
+
+        <div class="thumbnail-title">
+          <a href="/posts/{{ $post->id }}"><h2>{{ $post->title }}</h2></a>
+        </div>
       </article>
     @endforeach
     <div id="paginaton">
