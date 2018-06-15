0b9aa340252da5ec0ff56e89fe2416397d55b64f
Daiki Rokuyama <dr19950228@gmail.com>
2018-06-15 11:02:58 +0900

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,113 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Menu::truncate();
+        Menu::insert([
+            [
+                'parent_id' => 0,
+                'order'     => 1,
+                'title'     => 'Index',
+                'icon'      => 'fa-bar-chart',
+                'uri'       => '/',
+            ],
+            [
+                'parent_id' => 0,
+                'order'     => 2,
+                'title'     => 'Admin',
+                'icon'      => 'fa-tasks',
+                'uri'       => '',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 3,
+                'title'     => 'Users',
+                'icon'      => 'fa-users',
+                'uri'       => 'auth/users',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 4,
+                'title'     => 'Roles',
+                'icon'      => 'fa-user',
+                'uri'       => 'auth/roles',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 5,
+                'title'     => 'Permission',
+                'icon'      => 'fa-user',
+                'uri'       => 'auth/permissions',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 6,
+                'title'     => 'Menu',
+                'icon'      => 'fa-bars',
+                'uri'       => 'auth/menu',
+            ],
+            [
+                'parent_id' => 2,
+                'order'     => 7,
+                'title'     => 'Operation log',
+                'icon'      => 'fa-history',
+                'uri'       => 'auth/logs',
+            ],
+            [
+                'parent_id' => 0,
+                'order'     => 9,
+                'title'     => 'Helpers',
+                'icon'      => 'fa-gears',
+                'uri'       => '',
+            ],
+            [
+                'parent_id' => 8,
+                'order'     => 10,
+                'title'     => 'Scaffold',
+                'icon'      => 'fa-keyboard-o',
+                'uri'       => 'helpers/scaffold',
+            ],
+            [
+                'parent_id' => 8,
+                'order'     => 11,
+                'title'     => 'Database terminal',
+                'icon'      => 'fa-database',
+                'uri'       => 'helpers/terminal/database',
+            ],
+            [
+                'parent_id' => 8,
+                'order'     => 12,
+                'title'     => 'Laravel artisan',
+                'icon'      => 'fa-terminal',
+                'uri'       => 'helpers/terminal/artisan',
+            ],
+            [
+                'parent_id' => 0,
+                'order'     => 8,
+                'title'     => 'Contents',
+                'icon'      => 'fa-book',
+                'uri'       => '',
+            ],
+            [
+                'parent_id' => 12,
+                'order'     => 0,
+                'title'     => 'Posts',
+                'icon'      => 'fa-pencil',
+                'uri'       => 'posts',
+            ],
+            [
+                'parent_id' => 12,
+                'order'     => 0,
+                'title'     => 'Comments',
+                'icon'      => 'fa-commenting',
+                'uri'       => 'comments',
+            ],
+            [
+                'parent_id' => 12,
+                'order'     => 0,
+                'title'     => 'Categories',
+                'icon'      => 'fa-list-alt',
+                'uri'       => 'categories',
+            ],
         // $this->call(UsersTableSeeder::class);
     }
 }
