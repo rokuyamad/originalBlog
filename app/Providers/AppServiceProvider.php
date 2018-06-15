fb47cf6d36b29f811fede1979242eca35bf63e84
Daiki Rokuyama <dr19950228@gmail.com>
2018-06-15 15:03:11 +0900

    bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') !== 'local') {
-            $url->forceScheme('https');
+            $url->forceSchema('https');
         }
     }
 
