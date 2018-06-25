93ea3ce63ffb4e42ca2d33f6b4097dca8829d748
Daiki Rokuyama <dr19950228@gmail.com>
2018-06-25 14:48:19 +0900

    hoge

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -6,7 +6,7 @@ Admin::registerHelpersRoutes();
 
 Route::group([
     'prefix'        => config('admin.prefix'),
-    'namespace'     => Admin::controllerNamespace(),
+    // 'namespace'     => Admin::controllerNamespace(),
     'middleware'    => ['web', 'admin'],
 ], function (Router $router) {
     $router->get('/', 'App/Admin/Controllers/HomeController@index');
